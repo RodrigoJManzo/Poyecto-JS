6f42fc646481d2dde188a260cac3d731c3613151
RodrigoManzo <rodrigoj.manzo@gmail.com>
2022-05-05 21:21:26 -0300

    sigo arreglando estilos

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ let titulo = document.getElementById("modificable");
 titulo.innerText = `BIENVENIDO A NUESTRO SITIO ${bienvenida}`;
 
 
-let apodo = prompt("Cual es tu Apodo?");
+let apodo = prompt("Cual es tu Apodo?").toUpperCase();
 
 let mensajePersonal = document.createElement("h2");
 
-mensajePersonal.innerHTML = `<h2> Esperamos que hayas encontrado todo lo que buscabas y mucho mas ${apodo} </h2>`;
+mensajePersonal.innerHTML = `<h2> ESPERAMOS QUE HAYAS ENCONTRADO TODO LO QUE BUSCABAS Y MUCHO MAS ${apodo} </h2>`;
 
 document . body .append(mensajePersonal);
