f47f536a1923af4a384d134573b33c555413f1ab
RodrigoManzo <rodrigoj.manzo@gmail.com>
2022-05-06 22:57:42 -0300

    sigo arreglando carrito

--- store.js
+++ store.js
@@ -11,86 +11,126 @@ const productos = [
     {"nombre":"sportBag", "id": 8 , "precio": 469.99 }
 ]
 
+for(const {nombre: a, id: b} of productos){
+    let prodName = a;
+    let prodId = b;
+    console.log(`nombre ${a}, id ${b}`)
+}
+
 //variables
+
+const addToCart = document.getElementsByClassName("addToCart");
+
+
 const cart = document.querySelector("#cart");
+
 const agregadoCarrito = [];
 
-let btncart1 = document.querySelector("#btnCart1");
-let buscar1 = productos.find((el) => el.id === 1);
+//asignar funciones a los botones
 
-
-let btncart2 = document.querySelector("#btnCart2");
-let buscar2 = productos.find((el) => el.id === 2)
-
-let btncart3 = document.querySelector("#btnCart3");
-let buscar3 = productos.find((el) => el.id === 3)
-
-
-let btncart4 = document.querySelector("#btnCart4");
-let buscar4 = productos.find((el) => el.id === 4)
-
-let btncart5 = document.querySelector("#btnCart5");
-let buscar5 = productos.find((el) => el.id === 5)
-
-let btncart6 = document.querySelector("#btnCart6");
-let buscar6 = productos.find((el) => el.id === 6)
-
-let btncart7 = document.querySelector("#btnCart7");
-let buscar7 = productos.find((el) => el.id === 7)
-
-let btncart8 = document.querySelector("#btnCart8");
-let buscar8 = productos.find((el) => el.id === 8)
-
-
-// Por cada boton, agrego una funcion para que encuentre el objeto por su id en el array
-
-let prodId1 = buscar1.id;
-let prodName1 = buscar1.nombre;
-let prodPrecio1 = buscar1.precio;
-
-let prodId2 = buscar2.id;
-let prodName2 = buscar2.nombre;
-let prodPrecio2 = buscar2.precio;
-
-let prodId3 = buscar3.id;
-let prodName3 = buscar3.nombre;
-let prodPrecio3 = buscar3.precio;
-
-let prodId4 = buscar4.id;
-let prodName4 = buscar4.nombre;
-let prodPrecio4 = buscar4.precio;
-
-let prodId5 = buscar5.id;
-let prodName5 = buscar5.nombre;
-let prodPrecio5 = buscar5.precio;
-
-let prodId6 = buscar6.id;
-let prodName6 = buscar6.nombre;
-let prodPrecio6 = buscar6.precio;
-
-let prodId7 = buscar7.id;
-let prodName7 = buscar7.nombre;
-let prodPrecio7 = buscar7.precio;
-
-let prodId8 = buscar8.id;
-let prodName8 = buscar8.nombre;
-let prodPrecio8 = buscar8.precio;
-
-btncart1.addEventListener("click", add = () => {agregadoCarrito.push(buscar1)});
-btncart2.addEventListener("click", add = () => {agregadoCarrito.push(buscar2)});
-btncart3.addEventListener("click", add = () => {agregadoCarrito.push(buscar3)});
-btncart4.addEventListener("click", add = () => {agregadoCarrito.push(buscar4)});
-btncart5.addEventListener("click", add = () => {agregadoCarrito.push(buscar5)});
-btncart6.addEventListener("click", add = () => {agregadoCarrito.push(buscar6)});
-btncart7.addEventListener("click", add = () => {agregadoCarrito.push(buscar7)});
-btncart8.addEventListener("click", add = () => {agregadoCarrito.push(buscar8)});
-
-// ver si los productos ya estan
-for(let i=0; i <agregadoCarrito.length; i++){
-    if(agregadoCarrito[i].getAttribute("id") === prodId1 || prodId2 || prodId3 || prodId4 || prodId5 || prodId6 || prodId7 || prodId8){
-    swal(`este producto ya esta en su carro`);        
-    }
+for(let i=0; i < addToCart.length; i++){
+    let boton = addToCart[i];
+    boton.addEventListener("click", agregarCarrito);
 }
+
+function agregarCarrito(e){
+    for(let i=0; i < addToCart.length; i++){
+    let boton = addToCart[i];
+    let cartItem = addToCart.find((el) => el.id === boton.id)}
+    console.log(cartItem);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cart = document.querySelector("#cart");
+// const agregadoCarrito = [];
+
+// let btncart1 = document.querySelector("#btnCart1");
+// let buscar1 = productos.find((el) => el.id === 1);
+
+
+// let btncart2 = document.querySelector("#btnCart2");
+// let buscar2 = productos.find((el) => el.id === 2)
+
+// let btncart3 = document.querySelector("#btnCart3");
+// let buscar3 = productos.find((el) => el.id === 3)
+
+
+// let btncart4 = document.querySelector("#btnCart4");
+// let buscar4 = productos.find((el) => el.id === 4)
+
+// let btncart5 = document.querySelector("#btnCart5");
+// let buscar5 = productos.find((el) => el.id === 5)
+
+// let btncart6 = document.querySelector("#btnCart6");
+// let buscar6 = productos.find((el) => el.id === 6)
+
+// let btncart7 = document.querySelector("#btnCart7");
+// let buscar7 = productos.find((el) => el.id === 7)
+
+// let btncart8 = document.querySelector("#btnCart8");
+// let buscar8 = productos.find((el) => el.id === 8)
+
+
+// // Por cada boton, agrego una funcion para que encuentre el objeto por su id en el array
+
+// let prodId1 = buscar1.id;
+// let prodName1 = buscar1.nombre;
+// let prodPrecio1 = buscar1.precio;
+
+// let prodId2 = buscar2.id;
+// let prodName2 = buscar2.nombre;
+// let prodPrecio2 = buscar2.precio;
+
+// let prodId3 = buscar3.id;
+// let prodName3 = buscar3.nombre;
+// let prodPrecio3 = buscar3.precio;
+
+// let prodId4 = buscar4.id;
+// let prodName4 = buscar4.nombre;
+// let prodPrecio4 = buscar4.precio;
+
+// let prodId5 = buscar5.id;
+// let prodName5 = buscar5.nombre;
+// let prodPrecio5 = buscar5.precio;
+
+// let prodId6 = buscar6.id;
+// let prodName6 = buscar6.nombre;
+// let prodPrecio6 = buscar6.precio;
+
+// let prodId7 = buscar7.id;
+// let prodName7 = buscar7.nombre;
+// let prodPrecio7 = buscar7.precio;
+
+// let prodId8 = buscar8.id;
+// let prodName8 = buscar8.nombre;
+// let prodPrecio8 = buscar8.precio;
+
+// btncart1.addEventListener("click", add = () => {agregadoCarrito.push(buscar1)});
+// btncart2.addEventListener("click", add = () => {agregadoCarrito.push(buscar2)});
+// btncart3.addEventListener("click", add = () => {agregadoCarrito.push(buscar3)});
+// btncart4.addEventListener("click", add = () => {agregadoCarrito.push(buscar4)});
+// btncart5.addEventListener("click", add = () => {agregadoCarrito.push(buscar5)});
+// btncart6.addEventListener("click", add = () => {agregadoCarrito.push(buscar6)});
+// btncart7.addEventListener("click", add = () => {agregadoCarrito.push(buscar7)});
+// btncart8.addEventListener("click", add = () => {agregadoCarrito.push(buscar8)});
+
+// // ver si los productos ya estan
+// for(let i=0; i <agregadoCarrito.length; i++){
+//     if(agregadoCarrito[i].getAttribute("id") === prodId1 || prodId2 || prodId3 || prodId4 || prodId5 || prodId6 || prodId7 || prodId8){
+//     swal(`este producto ya esta en su carro`);        
+//     }
+// }
 
 
 
